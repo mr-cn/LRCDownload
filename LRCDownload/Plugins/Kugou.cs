a66575d026c4b47bd5aa17a2aa9bf0065de934fa
MR <19818121+mr-cn@users.noreply.github.com>
2018-12-15 23:25:46 +0800

    根据Resharper优化代码

---
+++ LRCDownload/Plugins/Kugou.cs
@@ -0,0 +1,18 @@
+﻿namespace LRCDownload.Plugins
+{
+    public class Kugou
+    {
+        public string name()
+        {
+            return "酷狗音乐";
+        }
+        public string author()
+        {
+            return "built-in";
+        }
+        public string version()
+        {
+            return "0.1";
+        }
+    }
+}
